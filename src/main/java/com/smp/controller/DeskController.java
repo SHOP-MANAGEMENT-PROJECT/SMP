4d0666296f79e70bd5b4d18c973614ff0af64e6b
AUlasEren <ulass.eren97@gmail.com>
2023-06-08 16:31:31 +0300

    controller eklendi 2

--- src/main/java/com/smp/controller/DeskController.java
+++ src/main/java/com/smp/controller/DeskController.java
@@ -4,13 +4,12 @@ import static com.smp.constants.ApiUrls.*;
 
 import com.smp.dto.request.NewCreateContentRequest;
 import com.smp.dto.request.NewCreateDeskRequest;
+import com.smp.dto.request.UpdateContentRequestDto;
+import com.smp.dto.request.UpdateDeskRequestDto;
 import com.smp.service.DeskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.smp.constants.ApiUrls.CONTENT;
 
@@ -25,4 +24,8 @@ public class DeskController {
         return ResponseEntity.ok(deskService.createDesk(dto));
     }
     //düzelttim
+    @PutMapping(UPDATE)
+    public ResponseEntity<Boolean> updateDesk(@RequestBody UpdateDeskRequestDto dto){
+        return ResponseEntity.ok(deskService.updateDesk(dto));
+    }
 }

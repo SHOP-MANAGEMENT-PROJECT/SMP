9b58c91a1da901bd0800cb58b35fb7ec876d0820
OgulcanTekin <ogulcantekin97@gmail.com>
2023-06-13 12:01:56 +0300

    13.06.2023

--- src/main/java/com/smp/controller/DeskController.java
+++ src/main/java/com/smp/controller/DeskController.java
@@ -6,10 +6,14 @@ import com.smp.dto.request.NewCreateContentRequest;
 import com.smp.dto.request.NewCreateDeskRequest;
 import com.smp.dto.request.UpdateContentRequestDto;
 import com.smp.dto.request.UpdateDeskRequestDto;
+import com.smp.repository.entity.Content;
+import com.smp.repository.entity.Desk;
 import com.smp.service.DeskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.smp.constants.ApiUrls.CONTENT;
 
@@ -27,5 +31,10 @@ public class DeskController {
     @PutMapping(UPDATE)
     public ResponseEntity<Boolean> updateDesk(@RequestBody UpdateDeskRequestDto dto){
         return ResponseEntity.ok(deskService.updateDesk(dto));
+    }
+    @GetMapping(FINDALL)
+    public ResponseEntity<List<Desk>> findAll() {
+
+        return ResponseEntity.ok(deskService.findAll());
     }
 }
